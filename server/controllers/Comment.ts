c1aee3b878f45c636661c1cf16bfc76ecad2dcc0
BVBFD <lsevina126@gmail.com>
2022-07-27 16:49:40 +0900

    comment controller

--- server/controllers/Comment.ts
+++ server/controllers/Comment.ts
@@ -1,13 +1,19 @@
-import { Router, Request, Response, NextFunction } from 'express';
+import { createError } from './../error';
+import { Request, Response, NextFunction } from 'express';
+import Comment from '../models/Comment';
+import Video from '../models/Video';
 
 export const addComment = async (
   req: Request,
   res: Response,
   next: NextFunction
 ) => {
+  const newComment = new Comment({ ...req.body, userId: req.user.id });
   try {
-  } catch (error) {
-    console.error(error);
+    const savedComment = await newComment.save();
+    res.status(200).json(savedComment);
+  } catch (err) {
+    next(err);
   }
 };
 
@@ -17,8 +23,16 @@ export const deleteComment = async (
   next: NextFunction
 ) => {
   try {
-  } catch (error) {
-    console.error(error);
+    const comment = await Comment.findById(req.params.id);
+    const video = await Video.findById(req.params.id);
+    if (req.user.id === comment?.userId || req.user.id === video?.userId) {
+      await Comment.findByIdAndDelete(req.params.id);
+      res.status(200).json('The comment has been deleted.');
+    } else {
+      return next(createError(403, 'You can delete only your comment!'));
+    }
+  } catch (err) {
+    next(err);
   }
 };
 
@@ -28,7 +42,9 @@ export const getComments = async (
   next: NextFunction
 ) => {
   try {
-  } catch (error) {
-    console.error(error);
+    const comments = await Comment.find({ videoId: req.params.videoId });
+    res.status(200).json(comments);
+  } catch (err) {
+    next(err);
   }
 };
