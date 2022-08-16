c18f74a095e2aebec8b59249768ea212023706b3
BVBFD <lsevina126@gmail.com>
2022-08-16 22:18:56 +0900

    add comments box

--- client/src/components/Comment.tsx
+++ client/src/components/Comment.tsx
@@ -1,10 +1,52 @@
 import React from 'react';
+import styled from 'styled-components';
+
+const Container = styled.div`
+  display: flex;
+  gap: 10px;
+  margin: 30px 0px;
+`;
+
+const Avatar = styled.img`
+  width: 50px;
+  height: 50px;
+  border-radius: 50%;
+`;
+
+const Details = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 10px;
+  color: ${({ theme }) => theme.text};
+`;
+
+const Name = styled.span`
+  font-size: 13px;
+  font-weight: 500;
+`;
+
+const Date = styled.span`
+  font-size: 12px;
+  font-weight: 400;
+  color: ${({ theme }) => theme.textSoft};
+  margin-left: 5px;
+`;
+
+const Text = styled.span`
+  font-size: 14px;
+`;
 
 const Comment = () => {
   return (
-    <div>
-      <span>Comment</span>
-    </div>
+    <Container>
+      <Avatar />
+      <Details>
+        <Name>
+          Lee Seong Eun<Date>1 day ago</Date>
+        </Name>
+        <Text>This is my Youtube Channel</Text>
+      </Details>
+    </Container>
   );
 };
 
