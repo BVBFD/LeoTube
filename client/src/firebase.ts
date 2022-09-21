7abf6ca0b932ed9850e42d56f0a823e6fe5ae234
BVBFD <lsevina126@gmail.com>
2022-09-22 01:33:38 +0900

    secetkey hided in env

--- client/src/firebase.ts
+++ client/src/firebase.ts
@@ -8,13 +8,13 @@ import { getAuth, GoogleAuthProvider } from 'firebase/auth';
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: 'AIzaSyDasbZ3tu8rGPIHbuWKvDNUqK9ZS6yloNw',
-  authDomain: 'leotube-35b74.firebaseapp.com',
-  projectId: 'leotube-35b74',
-  storageBucket: 'leotube-35b74.appspot.com',
-  messagingSenderId: '495443390630',
-  appId: '1:495443390630:web:20ff32f418f3fcc62ac467',
-  measurementId: 'G-078QH07Z9Z',
+  apiKey: `${process.env.REACT_APP_DOT_ENV_apiKey}`,
+  authDomain: `${process.env.REACT_APP_DOT_ENV_authDomain}`,
+  projectId: `${process.env.REACT_APP_DOT_ENV_projectId}`,
+  storageBucket: `${process.env.REACT_APP_DOT_ENV_storageBucket}`,
+  messagingSenderId: `${process.env.REACT_APP_DOT_ENV_messagingSenderId}`,
+  appId: `${process.env.REACT_APP_DOT_ENV_appId}`,
+  measurementId: `${process.env.REACT_APP_DOT_ENV_measurementId}`,
 };
 
 // Initialize Firebase
